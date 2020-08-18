8bb5480f5b46435e20264bacd68cea85ae41fa4d
Tuyana Tsydenova <tsidenovatb@gmail.com>
2020-08-18 22:41:50 +0400

    save

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-fetch('https://api.github.com/users/tsidenova')
-	.then(res => res.json())
+let preload = document.getElementById('preloader');
+let main = document.getElementById('main');
+
+setTimeout(() => {
+	preload.classList.add('text');
+	main.classList.remove('text');
+	document.body.style.background = "white";
+}, 4000);
+
+let showTime = new Promise((resolve, reject) => {
+	let timeNow = document.getElementById('time');
+	let time = new Date();
+	let day = time.getDate();
+	let month = time.getMonth();
+	let year = time.getFullYear();
+	let fullDate = day + "." + month + "." + year;
+	setTimeout(() => fullDate ? resolve(fullDate) : reject('Время неизвестно'), 2000);
+	timeNow.append(fullDate);
+});
+
+const theName = 'tsidenova';
+const url = 'https://api.github.com/users/';
+
+let getUser = fetch(`${url}${theName}`);
+
+Promise.all([getUser, showTime])
+  .then(([infoFromPromise, dateFromPromise]) => {
+    userInfo = infoFromPromise;
+    nowDate = dateFromPromise;
+  })
+  .then(res => userInfo.json())
 	.then(json => {
+
 		let linkProfile = document.getElementById('linkprofile');
 		console.log(linkprofile);
 		let name = json.name;
@@ -10,7 +40,8 @@ fetch('https://api.github.com/users/tsidenova')
 		myName.href = page;
 		myName.innerHTML = name;
 		linkProfile.append(myName);
-		
+
+
 		let bio = document.getElementById('bio');
 		let info = json.bio;
 		let myInfo = document.createElement('p');
